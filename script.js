244e268ae679efaa2c2cd189ce9081dc34dcd152
Elvis <heme1120242@gmail.com>
2024-11-20 09:22:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//object
+const person = {
+  firstName: "Elvis",
+  lastName: "Hansen",
+  age: 50,
+  hobbies: ["stealing cars", "robbing bank"],
+  city: "bergen",
+};
+
+console.log(person);
+console.log(person.hobbies[0]); //stealing cars
